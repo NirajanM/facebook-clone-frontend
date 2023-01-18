ddaccd48534177f3894a0e652ab190b85970a71b
Nirajan Malla <nirajannitro5@gmail.com>
2023-01-18 10:02:44 +0545

    created some stories and added horizontal scroll

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,19 +1,43 @@
 import Profile from "../assets/profile.webp";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import './Stories.css'
+import Story from "./Story";
 interface IStoriesProps {
 }
 
 const Stories: React.FunctionComponent<IStoriesProps> = (props) => {
+    const stories = [
+        {
+            image: "https://media.allure.com/photos/625449a82c7474034bf4b5f8/2:3/w_1332,h_1998,c_limit/blackpink%20jennie%20kim.jpg",
+            name: "Jenny Kim"
+        },
+        {
+            image: "https://i.pinimg.com/originals/c2/81/e8/c281e883fcd89408574e6edddba374fc.png",
+            name: "Elon Musk"
+        },
+        {
+            image: "https://i.pinimg.com/originals/8b/c7/d6/8bc7d63ea6ecb98eb6dd9319f7295fdc.jpg",
+            name: "Cristiano Ronaldo"
+        },
+        {
+            image: "https://i.pinimg.com/originals/5d/e0/4f/5de04f0d93fb225dfc4cf55969c3dd0d.jpg",
+            name: "Lionel Messi"
+        }
+    ]
     return (
-        <div className='flex overflow-x-scroll text-center border-y-4 border-slate-300 p-2 '>
-            <div className='text-center overflow-hidden h-40 rounded-xl border-2 w-24'>
+        <div className='flex overflow-x-auto text-center gap-2 border-y-4 border-slate-300 p-2 '>
+            <div className='text-center flex-shrink-0 overflow-hidden h-40 rounded-xl border-2 w-24'>
                 <img src={Profile} alt="profile" height={28} />
                 <div className="flex flex-col items-center">
                     <AddCircleIcon color="primary" className="border-2 bg-white border-white rounded-full" id="circle" />
                     <span className="px-4 font-black text-xs text-slate-800">Add to story</span>
                 </div>
             </div>
+            {stories.map((sto, key) => {
+                return (
+                    <Story image={sto.image} name={sto.name} key={key} />
+                );
+            })}
         </div>
     );
 };
--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,11 +1,13 @@
-import Profile from "../assets/profile.webp";
 interface IStoryProps {
+    image: string;
+    name: string;
 }
 
 const Story: React.FunctionComponent<IStoryProps> = (props) => {
     return (
-        <div className='overflow-hidden bg-slate-700 flex flex-col items-center justify-center h-40 rounded-xl border-2 w-24'>
-            <img src={Profile} alt="profile" width="full" />
+        <div className='overflow-hidden flex-shrink-0 bg-slate-800 flex flex-col justify-between h-40 rounded-xl border-2 w-24'>
+            <img src={props.image} alt="profile" width={96} className="min-w-24" />
+            <span className="text-xs text-white relative bottom-1 whitespace-nowrap overflow-x-hidden">{props.name}</span>
         </div>
     );
 };
