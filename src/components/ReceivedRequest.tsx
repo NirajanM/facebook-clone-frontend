f9765afd731345076142980935ce90d983d31cba
Nirajan Malla <nirajannitro5@gmail.com>
2023-01-21 18:46:40 +0545

    allignment fixed in received request

--- src/components/ReceivedRequest.tsx
+++ src/components/ReceivedRequest.tsx
@@ -10,7 +10,7 @@ const ReceivedRequest: React.FunctionComponent<IReceivedRequestProps> = (props) 
         <div className='border-y p-y-2'>
             <div className='flex items-center gap-1'>
                 <img src={props.image} alt={props.name + "profile"} className="w-20 rounded-full p-1" />
-                <div className='flex flex-col justify-center gap-2 w-full'>
+                <div className='flex flex-col justify-center w-full'>
                     <span className='text-md font-medium'>{props.name}</span>
                     <div className='flex font-bold text-sm justify-start w-full gap-2 text-center'>
                         <span className='text-white bg-blue-500 py-1 rounded-sm w-full'>Confirm</span>
