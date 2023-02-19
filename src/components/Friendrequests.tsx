b465b68897b46573246a41d1277c4d36c487f8d9
Nirajan Malla <nirajannitro5@gmail.com>
2023-02-19 21:44:53 +0545

    friendrequest update in style and json

--- src/components/Friendrequests.tsx
+++ src/components/Friendrequests.tsx
@@ -6,16 +6,19 @@ interface IFriendrequestsProps {
 const Friendrequests: React.FunctionComponent<IFriendrequestsProps> = (props) => {
     const receivedReqData = [
         {
-            profile: "https://scontent.fsif1-1.fna.fbcdn.net/v/t1.6435-9/172204944_4824527224246699_6918722293347789546_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=1&ccb=1-7&_nc_sid=85a577&efg=eyJpIjoidCJ9&_nc_ohc=uoR8L9ZRJ50AX-5EE3H&tn=SreNJeRuUc7uNJGc&_nc_ht=scontent.fsif1-1.fna&oh=00_AfDuADxuOq71y0_6Oug9eSYKUOeMOrMJ2l1VydRdkl2QHQ&oe=63EED47B",
+            profile: "/1.webp",
             name: "Routine Of Nepal Banda",
+            mutual: 4,
         },
         {
-            profile: "https://scontent.fsif1-1.fna.fbcdn.net/v/t1.6435-9/190108336_322648402555040_2100790391455013605_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=9XlfE5C5MnsAX8ocxfu&tn=lbg-XjLo8IK3Lred&_nc_ht=scontent.fsif1-1.fna&oh=00_AfCQpEGNS_8BNGhrHmZnIn8utG-JXibE32jSjnkhGzYgAA&oe=63F1DA00",
+            profile: "/3.webp",
             name: "Cristiano Ronaldo",
+            mutual: 10,
         },
         {
-            profile: "https://scontent.fsif1-1.fna.fbcdn.net/v/t1.6435-9/148677740_271496114335795_4694665719681802490_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mMTxf5z3PCcAX8XDVQh&_nc_ht=scontent.fsif1-1.fna&oh=00_AfA8sNVjzrUhnZEScMM4vBncnBXq9v0BWrvK-8F7Moekkw&oe=63F1DE29",
+            profile: "/4.webp",
             name: "Nepathya",
+            mutual: 122,
         },
     ]
 
@@ -27,7 +30,7 @@ const Friendrequests: React.FunctionComponent<IFriendrequestsProps> = (props) =>
             <div className='text-lg font-black py-2 border-y'>Friend Requests</div>
             {receivedReqData.map((eachRequest) => {
                 return (
-                    <ReceivedRequest image={eachRequest.profile} name={eachRequest.name} />
+                    <ReceivedRequest image={eachRequest.profile} name={eachRequest.name} mutual={eachRequest.mutual} />
                 )
             })}
         </div>
--- src/components/ReceivedRequest.tsx
+++ src/components/ReceivedRequest.tsx
@@ -3,24 +3,30 @@ import { useState } from 'react';
 interface IReceivedRequestProps {
     image: string;
     name: string;
+    mutual: number;
 }
 
 const ReceivedRequest: React.FunctionComponent<IReceivedRequestProps> = (props) => {
     const [accepted, setAccepted] = useState<null | boolean>(null);
     return (
-        <div className={(accepted == true || accepted == false) ? "bg-yellow-400/10" : "" + `border-y p-y-2`} >
+        <div className={(accepted == true || accepted == false) ? "bg-yellow-400/10" : "" + `border-y py-2 my-2`} >
             <div className='flex items-center gap-1'>
                 <img src={props.image} alt={props.name + "profile"} className="w-20 rounded-full p-1" />
 
                 <div className="flex flex-col justify-center w-full">
                     <span className='text-md font-medium'>{props.name}</span>
+
                     {accepted == null &&
-                        <div className='flex font-bold text-sm justify-start w-full gap-2 text-center'>
-                            <span className='text-white bg-blue-500 py-1 rounded-sm w-full'
-                                onClick={() => { setAccepted(true) }}>Confirm</span>
-                            <span className='text-black bg-slate-200 py-1 rounded-sm w-full'
-                                onClick={() => { setAccepted(false) }}>Reject</span>
-                        </div>}
+                        <>
+                            <span>{props.mutual} mutual friends</span>
+                            <div className='flex font-bold text-sm justify-start w-full gap-2 text-center'>
+                                <span className='text-white bg-blue-500 py-1 rounded-sm w-full'
+                                    onClick={() => { setAccepted(true) }}>Confirm</span>
+                                <span className='text-black bg-slate-200 py-1 rounded-sm w-full'
+                                    onClick={() => { setAccepted(false) }}>Reject</span>
+                            </div>
+                        </>
+                    }
 
                     {accepted ?
                         <span className='text-md font-medium'>Request Accepted</span>
