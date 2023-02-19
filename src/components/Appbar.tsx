3b554e01fb7732173b58e5c6fb68f54368cc33c5
Nirajan Malla <nirajannitro5@gmail.com>
2023-02-19 21:55:41 +0545

    added logout instead of menu

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,6 +1,6 @@
-import MenuIcon from '@mui/icons-material/Menu';
+import LogoutIcon from '@mui/icons-material/Logout';
 import SearchIcon from '@mui/icons-material/Search';
-
+import { Link } from 'react-router-dom';
 export default function SearchAppBar() {
     return (
         <div className='flex justify-between items-center px-3 py-1'>
@@ -9,7 +9,9 @@ export default function SearchAppBar() {
             </span>
             <div className='flex gap-2'>
                 <SearchIcon className='rounded-full bg-slate-200 p-1' fontSize='large' onClick={() => { alert("xaina data kei xaddeu") }} />
-                <MenuIcon fontSize='large' className='rounded-full bg-slate-200 p-1' />
+                <Link to="/">
+                    <LogoutIcon fontSize='large' className='rounded-full bg-slate-200 p-2' />
+                </Link>
             </div>
         </div>
     );
